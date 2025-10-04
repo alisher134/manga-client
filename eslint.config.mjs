2b02e23b93ce405ca5dc9eef3a7ee31b9783dfb7
alisher134 <alisherrakhmanov134@gmail.com>
2025-10-05 00:22:32 +0500

    chore(linter): add configs for prettier, eslint and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPlugin from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,15 +12,128 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    plugins: {
+      prettier: prettierPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-alert': 'warn',
+      'prefer-const': 'warn',
+      eqeqeq: ['error', 'always'],
+      'no-shadow': 'error',
+      'max-params': ['warn', 4],
+      'max-depth': ['warn', 4],
+      'prefer-template': 'error',
+      'max-lines': [
+        'warn',
+        {
+          max: 300,
+          skipBlankLines: true,
+          skipComments: true,
+        },
+      ],
+      'prefer-destructuring': [
+        'error',
+        {
+          array: false,
+          object: true,
+        },
+        {
+          enforceForRenamedProperties: false,
+        },
+      ],
+      'max-len': ['warn', { code: 130, ignoreStrings: true }],
+      'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0 }],
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        {
+          blankLine: 'always',
+          prev: ['const', 'let', 'var'],
+          next: '*',
+        },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+        { blankLine: 'always', prev: ['case', 'default'], next: '*' },
+        { blankLine: 'always', prev: ['export'], next: '*' },
+      ],
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: false,
+        },
+      ],
+
+      'prettier/prettier': ['error'],
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/jsx-sort-props': 'error',
+      'react/no-array-index-key': 'warn',
+      'react/self-closing-comp': 'error',
+      'react/jsx-key': [
+        'error',
+        {
+          checkFragmentShorthand: true,
+        },
+      ],
+      'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/jsx-no-duplicate-props': 'error',
+
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            ['^@?\\w'],
+
+            ['^@/api'],
+            ['^@/app'],
+            ['^@/assets'],
+
+            ['^@/components/pages'],
+            ['^@/components/layouts'],
+            ['^@/components/widgets'],
+            ['^@/components/wrappers'],
+            ['^@/components/dummies'],
+            ['^@/components/ui'],
+
+            ['^@/constants'],
+            ['^@/hooks'],
+            ['^@/models'],
+            ['^@/stores'],
+            ['^@/utils'],
+
+            ['^@/styles'],
+            ['.*\\.(css|scss)$'],
+          ],
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'eslint.config.mjs'],
   },
 ];
 
